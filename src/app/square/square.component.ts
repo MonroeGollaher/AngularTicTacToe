07de7fcc812c2c66728e74602646deb62696ffc5
Monroe Gollaher <mgollaher92@gmail.com>
2021-04-06 11:48:50 -0600

    completed, nebular styles

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,14 +2,14 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  templateUrl: './square.component.html',
-  styleUrls: ['./square.component.scss']
+  template: `
+    <button nbButton *ngIf="!value">{{ value }}</button>
+    <button nbButton hero status="success" *ngIf="value == 'X'">{{ value }}</button>
+    <button nbButton hero status="info" *ngIf="value == 'O'">{{ value }}</button>
+  `,
+  styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }']
 })
-export class SquareComponent {
-
-  constructor(){
-    console.log(this.value);
-  }
+export class SquareComponent  {
 
   @Input() value: 'X' | 'O';
 
